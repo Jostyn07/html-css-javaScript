fb24df13a6e2d5f8fc68884636c05b6b1a9fcd52
Jostyn07 <jostynag075@gmail.com>
2025-10-21 12:09:19 -0500

    Add files via upload

---
+++ FullStack/Sintaxis del lenguaje/Sintaxis palabras reservadas/2.3-operadores.js
@@ -0,0 +1,18 @@
+// Operadores de manejo de cadenas (string)
+var cadena = "lenguaje" + " " + "JavaScript";
+console.log(cadena)
+
+var cadena2 = "lenguaje";
+cadena2 += " ";
+cadena2 += "JavaScript";
+
+console.log(cadena2)
+
+// Operadores de tipo
+
+color1 = new String("Verde");
+color1 instanceof String
+console.log(color1);
+color2 = "Coral";
+color2 instanceof String;
+console.log(color2)
