c8a4e6be2217f14fafe6d5bd119e84db7e9c72f4
Jostyn07 <jostynag075@gmail.com>
2025-10-24 18:01:07 -0500

    Actualización

--- FullStack/3-Utilizacion_de_los_operadores/3.7-Objeto-string.js
+++ FullStack/3-Utilizacion_de_los_operadores/3.7-Objeto-string.js
@@ -15,3 +15,29 @@ text[4];
 var cadena = "Hola";
 for (i=0; i < cadena.length; i++)
     console.log(cadena[i]);
+
+// Interpolación de variables
+let nombre = "Juan";
+let mensaje = "Hola, " + nombre + "!"
+
+console.log(mensaje);
+
+let nombre1 = "Juan"
+let mensaje1 = `Hola, ${nombre1}!`;
+console.log(mensaje1);
+
+let numero = 5;
+let mensaje2 = `El doble de ${numero} es ${numero * 2}`
+console.log(mensaje2)
+
+// Substrings y posiciones
+
+// .charAt(pos): devuelve el caracter en la posicion pos. Es equivalente al operador [pos]
+// .indexOf(text): Devuelve la primera posicion del texto *text*
+// indexOf(text,desde): Devuelve la primra posicion de texto text, pero empezando a buscar desde la posicion *desde*
+// .lastIndex(text): Devuelve la última posicion del texto text
+// .lastIndex(text,desde): Devuelve la última posicion del texto text esde la posicion desde hasta el princpio
+
+const nombre2 = "Javi";
+console.log(nombre.charAt());
+console.log(nombre.charAt(0));
